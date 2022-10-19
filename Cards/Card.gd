e597224fbf8ba41506dbe7a89f016564f4c1aa13
Rafael Epplée <hello@rafa.ee>
2022-10-19 15:01:55 +0200

    move cards back to original position

--- Cards/Card.gd
+++ Cards/Card.gd
@@ -3,7 +3,9 @@ extends Sprite2D
 ## The duration of the scaling animation in seconds
 const SCALE_TWEEN_DURATION := 0.07
 ## The higher this number is, the faster the card follows the user's finger
-const MOVEMENT_SMOOTHING_SPEED := 55.0
+const DRAG_SMOOTH_SPEED := 55.0
+## Same as `DRAG_SMOOTH_SPEED`, but used when the user is not actively dragging
+const ANIMATE_SMOOTH_SPEED := 15.0
 
 ## Used to detect when users touch this card
 @onready var touch_area: Area2D = $TouchDetectionArea
@@ -18,6 +20,10 @@ var dragging := false
 
 ## Where on the card the user started dragging
 var drag_offset := Vector2.ZERO
+
+## Global position that this card had in the deck,
+## used to move the card back if the player doesn't choose it for playing
+@onready var starting_position := self.global_position
 
 ## The current position of the user's finger.
 ## in _process(), we interpolate the card position towards this position
@@ -36,10 +42,13 @@ func area_input(_viewport, event, _shape_index):
 			# The user wants to drag this card somewhere
 			self.dragging = true
 			self.drag_offset = self.global_position - event.position
+			# If the card is just returning to the hand, cancel that motion immediately
+			self.move_to(self.global_position)
 			create_tween().tween_property(self, "scale", Vector2.ONE, SCALE_TWEEN_DURATION)
 		else:
 			# The user stopped dragging this card
 			self.dragging = false
+			self.move_to(self.starting_position)
 			create_tween().tween_property(self, "scale", original_scale, SCALE_TWEEN_DURATION)
 
 ## Called for all input events, regardless of the area they're touching
@@ -60,6 +69,7 @@ func set_card_type(new_suite: Cards.Suite, new_number: Cards.Number):
 	self.texture = Cards.textures[self.suite][self.number]
 
 func _process(delta):
+	var smooth_speed = DRAG_SMOOTH_SPEED if self.dragging else ANIMATE_SMOOTH_SPEED
 	# improved lerp smoothing to make drag motion less jittery
 	# see https://www.gamedeveloper.com/programming/improved-lerp-smoothing- for an explanation of the formula
-	self.global_position = self.target_drag_position.lerp(self.global_position, pow(2, -delta * MOVEMENT_SMOOTHING_SPEED))
+	self.global_position = self.target_drag_position.lerp(self.global_position, pow(2, -delta * smooth_speed))

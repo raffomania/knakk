76dff005c89587ff6cc9a8b7310e2b32f272099d
Rafael Epplée <hello@rafa.ee>
2022-10-27 19:18:58 +0200

    better comments

--- Events.gd
+++ Events.gd
@@ -4,9 +4,9 @@ extends Node
 ## All the card types currently available to play
 ## This is not optimal because we have to take care to update this
 ## when something changes in `Card.gd`, which is error prone.
-## But since we need this in various parts of the field to compute
-## which slots to highlight, it's better to have this here
-## than to extract it from the Hand when we need it
+## But since we need this in different parts of the game,
+## it's better to have this here than to refer to 
+# the Hand everywhere we need it
 var card_types_in_hand: Array[Array]
 
 signal consider_card(card_type)

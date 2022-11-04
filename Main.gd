ac299aa65e11385efea2460e125bb6b7c848e504
Rafael Epplée <hello@rafa.ee>
2022-11-04 15:26:57 +0100

    refactor: Move main game into its own scene

--- Main.gd
+++ Main.gd
@@ -6,5 +6,9 @@ func _ready():
 func _game_over() -> void:
 	await $Camera.go_to_game_over_screen()
 	await get_tree().create_timer(0.5).timeout
-	var score = $TopBar/Score.score
+	var score = $GameScreen/TopBar/Score.score
 	$GameOverScreen.animate_score(score)
+
+func _unhandled_input(event):
+	if event.is_action_released("game_over") and OS.is_debug_build():
+		_game_over()

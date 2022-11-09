ca321975b9f24ce91722162e2b0252c4dc9bb9a2
Rafael Epplée <hello@rafa.ee>
2022-11-09 13:07:53 +0100

    feat: draw arrows for diamond slots

--- Field/Arrow.gd
+++ Field/Arrow.gd
@@ -12,24 +12,26 @@ var color := Color.BLACK:
 		if is_instance_valid(label):
 			label.add_theme_color_override("font_color", color)
 
+
+const START_TEXTURE = preload("res://Field/ArrowStart.svg")
+const END_TEXTURE = preload("res://Field/ArrowEnd.svg")
+		
 func _ready():
 	if is_instance_valid(label):
 		label.add_theme_color_override("font_color", color)
 	label.size = size
 	label.text = text
 
-# todo fix vertical drawing
-# func _draw():
-# 	draw_line(
-# 		Vector2(0, size.y / 2),
-# 		Vector2(size.x * 1/5, size.y / 2), 
-# 		color, 2.0, true)
-# 	draw_line(
-# 		Vector2(size.x * 4/5, size.y / 2), 
-# 		Vector2(size.x, size.y / 2), 
-# 		color, 2.0, true)
-# 	draw_line(
-# 		Vector2(size.x * 9/10, size.y * 2/5), 
-# 		Vector2(size.x - 2, size.y / 2), 
-# 		color, 2.0, true)
-# 	draw_line(Vector2(size.x * 9/10, size.y * 3/5), Vector2(size.x - 2, size.y / 2), color, 2.0, true)
+func _draw():
+	# To preserve the aspect ratio of the arrow, it is drawn in two parts, 
+	# each with a square size that scales to fit the overall arrow bounding box
+	var part_size = Vector2.ONE * min(size.x, size.y) * 0.4
+	if is_vertical:
+		# Draw parts centered on the vertical axis and rotate them
+		draw_set_transform(Vector2((size.x + part_size.x) / 2, 0), PI / 2)
+		draw_texture_rect(START_TEXTURE, Rect2(Vector2(0, 0), part_size), false)
+		draw_texture_rect(END_TEXTURE, Rect2(Vector2(size.y - part_size.y, 0), part_size), false)
+	else:
+		# Draw parts centered on the horizontal axis
+		draw_texture_rect(START_TEXTURE, Rect2(Vector2(0, (size.y - part_size.y) / 2), part_size), false)
+		draw_texture_rect(END_TEXTURE, Rect2(Vector2(size.x - part_size.x, (size.y - part_size.y) / 2), part_size), false)

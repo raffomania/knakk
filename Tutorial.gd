bc8fc24601fdc2b57af1e80492dd929ac6ff4fe1
Rafael Epplée <hello@rafa.ee>
2022-11-16 16:30:12 +0100

    feat: Better tutorial layout and aesthetics

--- Tutorial.gd
+++ Tutorial.gd
@@ -1,44 +1,31 @@
 extends Control
 
 const STEPS := [
-	"""
-Welcome to Clever Solitaire!
+	"""Welcome to Clever Solitaire!
 In this game, you play cards to fill slots.
 Each filled slot will reward you with points or bonuses. 
-The goal is to get the highest possible score!
-	""",
-	"""
-Each turn, you pick two cards: First, a [b]suite[/b] card. 
+The goal is to get the highest possible score!""",
+	"""Each turn, you pick two cards: First, a [b]suite[/b] card. 
 Its suite determines the area where you'll play the second card: 
 The [b]number[/b] card.  Its number determines the slot you're going to fill.
-Try playing a turn now!
-	""",
-	"""
-The turn indicator boxes at the top show how many turns you've played.
+You can drag cards upwards to play them. Try it now!""",
+	"""The turn indicator boxes at the top show how many turns you've played.
 After each turn, you get a new hand with three new cards.
 After five turns, the deck is refilled and shuffled.
-After fifteen turns, the game is over.
-	""",
-	"""
-Each suite has its own rules:
-[b]Clubs[/b]: Filled from left to right. Each slot needs a number higher than the previous one.
-[b]Diamonds[/b]: Filled from the top left corner. Each number has to fit the range indicated on the slot.
-	""",
-	"""
-[b]Spades[/b]: Filled in [b]any[/b] order. 
+After fifteen turns, the game is over.""",
+	"""Each suite has its own rules:
+[b]Clubs[/b]: Filled from left to right, with each number higher than the previous one.
+[b]Diamonds[/b]: Starting from the top left corner, numbers need to fit the range on each slot.""",
+	"""[b]Spades[/b]: Filled in [b]any[/b] order. 
 Each slot needs exactly the number it shows.
 [b]Hearts[/b]: Filled from left to right. 
-Each slot needs a number higher or equal to the one it shows.
-	""",
-	"""
-There are two bonuses:
+Each slot needs a number higher or equal to the one it shows.""",
+	"""There are two bonuses:
 [img color=#303030 height=35]res://Action//RedrawCard.svg[/img]: Replace a card with a new one by dragging it to the bottom left corner.
-[b]++[/b]: Play the same hand twice by dragging a card to the bottom right corner.
-	""",
-	"""
-	You're ready to play.
-	Good luck and have fun!
-	""",
+[b]++[/b]: Play the same hand twice by dragging a card to the bottom right corner.""",
+	"""You're ready to play.
+Good luck and have fun!
+""",
 ]
 
 var _current_step := 0
